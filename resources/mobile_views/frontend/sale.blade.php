10fd2d0eff0afcd7f786ba9f063ffb90ec74bb51
mostakbrandh <mostakbrandh@gmail.com>
2022-11-20 10:49:12 +0600

    --

--- resources/mobile_views/frontend/sale.blade.php
+++ resources/mobile_views/frontend/sale.blade.php
@@ -1,34 +1,23 @@
 @extends('frontend.layouts.app')
 
 @section('content')
-{{-- slider start--}}
-<div id="sliderimages">
-    @if (get_setting('home_slider_images') != null)
-        <div class="aiz-carousel dots-inside-bottom mobile-img-auto-height" data-dots="true" data-autoplay="true">
-            @php
-                $slider_images = Cache::get('home_slider_images')??null;
-            @endphp
-{{--                                @if($slider_images)--}}
+                           
                 <div class="carousel-box">
                     
                         <img
-                            class="d-block mw-100 img-fit rounded shadow-sm overflow-hidden"
+                            class="d-block mw-100 img-cover rounded shadow-sm overflow-hidden h-90px h-md-auto"
                             src="https://brandhook.s3.ap-south-1.amazonaws.com/uploads/all/8OL88IcuCmsbRd0fTwswp4R8Fl3B1S0sDMhtB220.png"
-                            data-src="{{ $slider_images[0]??"#"}}"
-                            alt="{{ env('APP_NAME')}} promo"
                            
-                            height="auto"
-                           
+                            alt="{{ env('APP_NAME')}} promo"                        
+                            
+                            width="100%"
                             onerror="this.onerror=null;this.src='{{ static_asset('assets/img/placeholder-rect.jpg') }}';"
                         >
                     
                 </div>
-{{--                                    @endif--}}
-        </div>
-    @endif
 
-</div>
-{{-- slider end--}}
+
+
 
 <section class="mb-4">
     <div class="container">
@@ -36,7 +25,7 @@
             <div class="">
                 <div class="mb-3 mb-lg-0">
                     <a href="" class="d-block text-reset mb-3 rounded" >
-                        <img src="https://brandhook.s3.ap-south-1.amazonaws.com/uploads/all/COBE6s0VhPslrXsOOQkU7NvZmEXEDq1r9JTEDJLx.webp" alt="{{ env('APP_NAME') }} promo" class="w-100 rounded img-fluid">
+                        <img src="https://brandhook.s3.ap-south-1.amazonaws.com/uploads/all/COBE6s0VhPslrXsOOQkU7NvZmEXEDq1r9JTEDJLx.webp" alt="{{ env('APP_NAME') }} promo" class="w-100 rounded h-70px h-md-auto">
                     </a>
                 </div>
             </div>
@@ -63,7 +52,7 @@
         </div>
         </div>
         <div class="text-center" id="smartbutton">
-            <button class="btn btn-md btn-primary" onclick="hideshow('smartwatch','smartbutton')" >Load More</button>
+            <button class="btn btn-sm btn-primary" onclick="hideshow('smartwatch','smartbutton')" >Load More</button>
         </div>
     
            
@@ -79,7 +68,7 @@
             <div class="">
                 <div class="mb-3 mb-lg-0">
                     <a href="" class="d-block text-reset mb-3 rounded" >
-                        <img src="https://brandhook.s3.ap-south-1.amazonaws.com/uploads/all/F2C1amz88qfjACY5nqZHsMX31tpm939Dq7SOs3Zz.webp" alt="{{ env('APP_NAME') }} promo" class="w-100 rounded img-fluid">
+                        <img src="https://brandhook.s3.ap-south-1.amazonaws.com/uploads/all/F2C1amz88qfjACY5nqZHsMX31tpm939Dq7SOs3Zz.webp" alt="{{ env('APP_NAME') }} promo" class="w-100 rounded h-70px h-md-auto">
                     </a>
                 </div>
             </div>
@@ -106,7 +95,7 @@
         </div>
         </div>
         <div class="text-center" id="musicbutton">
-            <button class="btn btn-md btn-primary" onclick="hideshow('music','musicbutton')" >Load More</button>
+            <button class="btn btn-sm btn-primary" onclick="hideshow('music','musicbutton')" >Load More</button>
         </div>
     
            
@@ -121,7 +110,7 @@
             <div class="">
                 <div class="mb-3 mb-lg-0">
                     <a href="" class="d-block text-reset mb-3 rounded" >
-                        <img src="https://brandhook.s3.ap-south-1.amazonaws.com/uploads/all/YjfQsClu6wuYaBYYNOb9xYAIjuZnVbKveVUtoHb9.webp" alt="{{ env('APP_NAME') }} promo" class="w-100 rounded img-fluid">
+                        <img src="https://brandhook.s3.ap-south-1.amazonaws.com/uploads/all/YjfQsClu6wuYaBYYNOb9xYAIjuZnVbKveVUtoHb9.webp" alt="{{ env('APP_NAME') }} promo" class="w-100 rounded h-70px h-md-auto">
                     </a>
                 </div>
             </div>
@@ -148,7 +137,7 @@
         </div>
         </div>
         <div class="text-center" id="soundbutton">
-            <button class="btn btn-md btn-primary" onclick="hideshow('sound','soundbutton')" >Load More</button>
+            <button class="btn btn-sm btn-primary" onclick="hideshow('sound','soundbutton')" >Load More</button>
         </div>
     
            
@@ -162,7 +151,7 @@
             <div class="">
                 <div class="mb-3 mb-lg-0">
                     <a href="" class="d-block text-reset mb-3 rounded" >
-                        <img src="https://brandhook.s3.ap-south-1.amazonaws.com/uploads/all/txsZaHltOfO7J6XiIUgHKO5mF6p64VCsuGjzCnqD.webp" alt="{{ env('APP_NAME') }} promo" class="w-100 rounded img-fluid">
+                        <img src="https://brandhook.s3.ap-south-1.amazonaws.com/uploads/all/txsZaHltOfO7J6XiIUgHKO5mF6p64VCsuGjzCnqD.webp" alt="{{ env('APP_NAME') }} promo" class="w-100 rounded h-70px h-md-auto">
                     </a>
                 </div>
             </div>
@@ -189,7 +178,7 @@
         </div>
         </div>
         <div class="text-center" id="powerbankbutton">
-            <button class="btn btn-md btn-primary" onclick="hideshow('powerbank','powerbankbutton')" >Load More</button>
+            <button class="btn btn-sm btn-primary" onclick="hideshow('powerbank','powerbankbutton')" >Load More</button>
         </div>
     
            
@@ -206,7 +195,7 @@
             <div class="">
                 <div class="mb-3 mb-lg-0">
                     <a href="" class="d-block text-reset mb-3 rounded" >
-                        <img src="https://brandhook.s3.ap-south-1.amazonaws.com/uploads/all/n0FnsGaumWeh4K32Eoj1dzS5Ijsxzu5KXdtT2U5I.webp" alt="{{ env('APP_NAME') }} promo" class="w-100 rounded img-fluid">
+                        <img src="https://brandhook.s3.ap-south-1.amazonaws.com/uploads/all/n0FnsGaumWeh4K32Eoj1dzS5Ijsxzu5KXdtT2U5I.webp" alt="{{ env('APP_NAME') }} promo" class="w-100 rounded h-70px h-md-auto">
                     </a>
                 </div>
             </div>
@@ -233,7 +222,7 @@
         </div>
         </div>
         <div class="text-center" id="smartvsbutton">
-            <button class="btn btn-md btn-primary" onclick="hideshow('smartvs','smartvsbutton')" >Load More</button>
+            <button class="btn btn-sm btn-primary" onclick="hideshow('smartvs','smartvsbutton')" >Load More</button>
         </div>
     
            
@@ -252,7 +241,7 @@
             <div class="">
                 <div class="mb-3 mb-lg-0">
                     <a href="" class="d-block text-reset mb-3 rounded" >
-                        <img src="https://brandhook.s3.ap-south-1.amazonaws.com/uploads/all/uLfN5lT96ACAAwDGhQ6qJ3v2p5zIGHBWCp7i2pAs.webp" alt="{{ env('APP_NAME') }} promo" class="w-100 rounded img-fluid">
+                        <img src="https://brandhook.s3.ap-south-1.amazonaws.com/uploads/all/uLfN5lT96ACAAwDGhQ6qJ3v2p5zIGHBWCp7i2pAs.webp" alt="{{ env('APP_NAME') }} promo" class="w-100 rounded h-70px h-md-auto">
                     </a>
                 </div>
             </div>
@@ -279,13 +268,14 @@
         </div>
         </div>
         <div class="text-center" id="fragrancesbutton">
-            <button class="btn btn-md btn-primary" onclick="hideshow('fragrances','fragrancesbutton')" >Load More</button>
+            <button class="btn btn-sm btn-primary" onclick="hideshow('fragrances','fragrancesbutton')" >Load More</button>
         </div>
     
            
         </div>
     </div>
 </section>
+<br><br>
 
 @section('script')
     <script type="text/javascript">
